019fca60a29f821b2f069fb40e089429591f5818
Sunhye <49827929+mocha-kim@users.noreply.github.com>
2021-04-06 17:40:47 +0900

    key_handling.c norm

--- srcs/key_handling.c
+++ srcs/key_handling.c
@@ -1,100 +1,84 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   key_handling.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sunhkim <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/01 15:15:20 by sunhkim           #+#    #+#             */
-/*   Updated: 2021/04/01 15:15:22 by sunhkim          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../includes/cub3d.h"
 
-void    move(t_info *info, int direction)
+void	move(t_info *info, int direction)
 {
-    int tmpX;
-    int tmpY;
+	int	tmp_x;
+	int	tmp_y;
 
-    tmpX = info->posX + direction * info->dirX * info->moveSpeed;
-    tmpY = info->posY + direction * info->dirY * info->moveSpeed;
-    if (direction == 1)
-    {
-        if (!info->map[tmpX][tmpY])
-        {
-            info->posX += info->dirX * info->moveSpeed;
-            info->posY += info->dirY * info->moveSpeed;
-        }
-    }
-    if (direction == -1)
-    {
-        if (!info->map[tmpX][tmpY])
-        {
-            info->posX -= info->dirX * info->moveSpeed;
-            info->posY -= info->dirY * info->moveSpeed;
-        }
-    }
+	tmp_x = info->posX + direction * info->dirX * info->moveSpeed;
+	tmp_y = info->posY + direction * info->dirY * info->moveSpeed;
+	if (direction == 1)
+	{
+		if (!info->map[tmp_x][tmp_y])
+		{
+			info->posX += info->dirX * info->moveSpeed;
+			info->posY += info->dirY * info->moveSpeed;
+		}
+	}
+	if (direction == -1)
+	{
+		if (!info->map[tmp_x][tmp_y])
+		{
+			info->posX -= info->dirX * info->moveSpeed;
+			info->posY -= info->dirY * info->moveSpeed;
+		}
+	}
 }
 
-void    rotate(t_info *info, int direction)
+void	rotate(t_info *info, int direction)
 {
-    double  oldDirX;
-    double  oldPlnX;
-    double  tmpX;
-    double  tmpY;
+	double	old_dir_x;
+	double	old_pln_x;
+	double	tmp_x;
+	double	tmp_y;
 
-    oldDirX = info->dirX;
-    tmpX = info->dirX * cos(direction * info->rotSpeed);
-    tmpY = info->dirY * sin(direction * info->rotSpeed);
-    info->dirX = tmpX - tmpY;
-    tmpX = oldDirX * sin(direction * info->rotSpeed);
-    tmpY = info->dirY * cos(direction * info->rotSpeed);
-    info->dirY = tmpX + tmpY;
-    oldPlnX = info->planeX;
-    tmpX = info->planeX * cos(direction * info->rotSpeed);
-    tmpY = info->planeY * sin(direction * info->rotSpeed);
-    info->planeX = tmpX - tmpY;
-    tmpX = oldPlnX * sin(direction * info->rotSpeed);
-    tmpY = info->planeY * cos(direction * info->rotSpeed);
-    info->planeY = tmpX + tmpY;
+	old_dir_x = info->dirX;
+	tmp_x = info->dirX * cos(direction * info->rotSpeed);
+	tmp_x = info->dirY * sin(direction * info->rotSpeed);
+	info->dirX = tmpX - tmpY;
+	tmp_x = old_dir_x * sin(direction * info->rotSpeed);
+	tmp_y = info->dirY * cos(direction * info->rotSpeed);
+	info->dirY = tmpX + tmpY;
+	old_pln_x = info->planeX;
+	tmp_x = info->planeX * cos(direction * info->rotSpeed);
+	tmp_y = info->planeY * sin(direction * info->rotSpeed);
+	info->planeX = tmpX - tmpY;
+	tmp_x = old_pln_x * sin(direction * info->rotSpeed);
+	tmp_y = info->planeY * cos(direction * info->rotSpeed);
+	info->planeY = tmpX + tmpY;
 }
 
-int     key_update(t_info *info)
+int		key_update(t_info *info)
 {
-    if (info->key.esc)
-        exit(0);
-    if (info->key.w)
-        move(info, 1);
-    if (info->key.s)
-        move(info, -1);
-    if (info->key.d)
-        rotate(info, -1);
-    if (info->key.a)
-        rotate(info, 1);
-    return (0);
+	if (info->key.w)
+		move(info, 1);
+	if (info->key.s)
+		move(info, -1);
+	if (info->key.d)
+		rotate(info, -1);
+	if (info->key.a)
+		rotate(info, 1);
+	return (0);
 }
 
-int     key_press(int key, t_info *info)
+int		key_press(int key, t_info *info)
 {
-    if (key == KEY_W || key == KEY_UP)
-        info->key.w = 1;
-    if (key == KEY_S || key == KEY_DOWN)
-        info->key.a = 1;
-    if (key == KEY_A || key == KEY_LEFT)
-        info->key.s = 1;
-    if (key == KEY_D || key == KEY_RIGHT)
-        info->key.d = 1;
-    if (key == KEY_ESC)
-        exit(0);
-    return (0);
+	if (key == KEY_W || key == KEY_UP)
+		info->key.w = 1;
+	if (key == KEY_S || key == KEY_DOWN)
+		info->key.a = 1;
+	if (key == KEY_A || key == KEY_LEFT)
+		info->key.s = 1;
+	if (key == KEY_D || key == KEY_RIGHT)
+		info->key.d = 1;
+	if (key == KEY_ESC)
+		main_close(info);
+	return (0);
 }
 
 int		key_release(int key, t_info *info)
 {
-	if (key == KEY_ESC)
-		exit(0);
-	else if (key == KEY_W || key == KEY_UP)
+	if (key == KEY_W || key == KEY_UP)
 		info->key.w = 0;
 	else if (key == KEY_A || key == KEY_DOWN)
 		info->key.a = 0;
