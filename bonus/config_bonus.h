eabd127fd65b79e80344e93a4e1f5d33449f9831
Ahn YoungSeon <dudtjs0920@naver.com>
2021-04-16 21:11:47 +0900

    variable name, func edit

--- bonus/config_bonus.h
+++ bonus/config_bonus.h
@@ -25,11 +25,11 @@
 # define T_SO 1
 # define T_WE 2
 # define T_EA 3
-# define T_SPRITE 4
+# define T_S 4
 # define T_SPRITE_UP 5
 # define T_SPRITE_C 6
-# define T_CEILING 7
-# define T_FLOOR 8
+# define T_C 7
+# define T_F 8
 
 typedef struct	s_config
 {
@@ -44,7 +44,6 @@ typedef struct	s_config
 	int			pos_x;
 	int			pos_y;
 	char		dir;
-	int			path_cnt;
 	int			set[11];
 }				t_config;
 
@@ -59,7 +58,7 @@ int				parse_texture(t_config *config, int id, char *line);
 int				str_to_color(char *line);
 int				parse_color(t_config *config, int id, char *line);
 
-void			player_set(t_config *config, int x, int y);
+int				player_set(t_config *config, int x, int y);
 int				parse_map(t_config *config, t_list *map_buffer);
 int	 			copy_map(t_config *config, t_list *map_buffer);
 int				check_map_row(t_list *map_buffer);
@@ -69,5 +68,6 @@ int				valid_map_check(t_config *config);
 int				element_check(int set[]);
 int				player_valid_check(t_config *config);
 int				rotate_map(t_config *config);
+int				notspace_index(char *line, int ind);
 
 #endif
