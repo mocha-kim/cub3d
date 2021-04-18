59fac881c80e9c117e5cb0fffcb17a42a3d0c365
Ahn YoungSeon <dudtjs0920@naver.com>
2021-04-19 00:45:04 +0900

    barrel - point, light - not collisions, pillar - collisions

--- bonus/score_bonus.c
+++ bonus/score_bonus.c
@@ -7,7 +7,9 @@ static void	calc_score(t_info *info)
 	i = 0;
 	while (i < info->sprite_num)
 	{
-		if ((int)info->posX == (int)info->sprite[i].x && (int)info->posY == (int)info->sprite[i].y)
+		if ((int)info->posX == (int)info->sprite[i].x
+		&& (int)info->posY == (int)info->sprite[i].y
+		&& info->sprite[i].texture == T_S)
 		{
 			info->sprite[i].x = info->sprite[info->sprite_num - 1].x;
 			info->sprite[i].y = info->sprite[info->sprite_num - 1].y;
@@ -26,7 +28,9 @@ void		draw_score(t_info *info)
 
 	calc_score(info);
 	score = ft_itoa(info->score);
-	mlx_string_put(info->mlx, info->win, info->conf.win_width - 100, info->conf.win_height - 10, 0xFFFFFF, "score : ");
-	mlx_string_put(info->mlx, info->win, info->conf.win_width - 45, info->conf.win_height - 10, 0xFFFFFF, score);
+	mlx_string_put(info->mlx, info->win, info->conf.win_width - 100,
+	info->conf.win_height - 10, 0xFFFFFF, "score : ");
+	mlx_string_put(info->mlx, info->win, info->conf.win_width - 45,
+	info->conf.win_height - 10, 0xFFFFFF, score);
 	free(score);
 }
